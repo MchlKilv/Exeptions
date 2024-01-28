85b008caa4ee2a72c2a9b54dd1bee35b818557d1
Mike <mchlkiselev@gmail.com>
2024-01-28 17:49:06 +0300

    Lect_1 add

--- Lect_1/Exeptions_Lect/src/App.java
+++ Lect_1/Exeptions_Lect/src/App.java
@@ -1,5 +1,54 @@
+import java.io.File;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        System.out.println(divide(10, 0));
+    }
+
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new RuntimeException("Делить на ноль нельзя!");
+        }
+        return a / b;
     }
 }
+// Пример для ошибок вызова метода getFileSize
+// System.out.println(getFileSize(new File("src/TestFil.txt")));
+// System.out.println(getFileSize(new File("src/TestFile.txt")));
+// System.out.println(divide(10, 0));
+
+// Пример для ошибок массива
+// Цепочка ошибок
+// a();
+// }
+
+// public static void a() {
+// b();
+// }
+
+// public static void b() {
+// c();
+// }
+
+// public static void c() {
+// int[] ints = new int[10];
+// System.out.println(ints[100]);
+// }
+// }
+
+// //Пример для ошибок вызова метода divide (деление на ноль)
+// public static int divide(int a, int b) {
+// if (b == 0) {
+// return -1;
+// }
+// return a / b;
+// }
+
+// Пример для ошибок вызова метода getFileSize
+// public static long getFileSize(File file) {
+// if (!file.exists()) {
+// return -1L;
+// }
+// return file.length();
+// }
